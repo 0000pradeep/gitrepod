8dbbe0e49c6625073526c85b0f078acf4244ae28
pradeep boolchandani <pradeepboolchandani81@gmail.com>
2018-07-07 16:31:29 +0530

    commiting

---
+++ MavenProject/src/test/java/Example/Test1.java
@@ -0,0 +1,16 @@
+package Example;
+
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	
+  @Test
+  public void f() {
+	  
+	 
+      System.out.println("Test Executed");
+    
+  }
+}
